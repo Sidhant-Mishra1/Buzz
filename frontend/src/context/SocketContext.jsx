c47da0091d148057d948ff70e02711fdcb369b96
Sidhant Mishra <Sidhantmishra101@gmail.com>
2024-06-20 11:54:13 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser}=useAuthContext()
     useEffect(()=>{
         if(authUser){
-            const socket=io('http://localhost:5001',{
+            const socket=io('https://buzz-myvl.onrender.com',{
                 query:{
                     userId:authUser._id
                 }
